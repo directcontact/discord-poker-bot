e49a28c87366a4b409057c544d1e83e9e93b71ff
timothychun <timothy.chun@yahoo.com>
2020-08-21 19:58:29 -0400

    adding my current changes

--- src/index.js
+++ src/index.js
@@ -4,6 +4,9 @@ require('dotenv').config();
 const sqlite3 = require('sqlite3').verbose();
 const constants = require('./constants/poker-constants');
 const { initializeDeck } = require('./poker');
+const dbcommands = require('./db-commands');
+const { profileExists } = require('./db-commands');
+const { ACE_OF_D } = require('./constants/poker-constants');
 const MAX_PLAYERS = 2;
 
 var gameState = {
@@ -19,13 +22,13 @@ let db = new sqlite3.Database(':memory:', (err) => {
 });
 
 db.run(
-  'CREATE TABLE users (id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE, name TEXT, chips INTEGER);',
+  'CREATE TABLE profiles (id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE, name TEXT, chips INTEGER);',
   (err) => {
     if (err) {
       return console.error(err.message);
     }
 
-    console.log('Created table users!');
+    console.log('Created table profiles!');
   }
 );
 // Login
@@ -49,6 +52,17 @@ client.on('message', (msg) => {
     // ch.send('The game will start!\nType !play to join!')
     gameState.status = true;
     //channel.send('The game will start! \n Type !play to join!')
+
+    db.run(
+      `CREATE TABLE users (id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE, name TEXT, chips INTEGER);`,
+      (err) => {
+        if (err) {
+          return console.log(err.message);
+        }
+
+        console.log('Created table users');
+      }
+    );
   }
 });
 
@@ -87,7 +101,7 @@ client.on('message', (msg) => {
                       console.log(err);
                     }
                     msg.channel.send(
-                      `(${num[Object.keys(num)[0]] + 1}/${MAX_PLAYERS})`
+                      `(${num[Object.keys(num)[0]] + 1}/${MAX_PLAYERS}) `
                     );
                     // msg.channel.send('You are the first to join!');
                   }
@@ -108,12 +122,9 @@ client.on('message', (msg) => {
 
 client.on('message', (msg) => {
   if (msg.content === '!count') {
-    db.get(`SELECT COUNT(DISTINCT name) FROM users;`, (err, num) => {
-      if (err) {
-        console.log(err.message);
-      }
-      msg.reply(`${msg.author.username}|${num[Object.keys(num)[0]]}`);
-    });
+    msg.reply(
+      `${msg.author.username}|${dbcommands.getChips(db, msg.author.username)}`
+    );
   }
 });
 
@@ -121,6 +132,82 @@ client.on('message', (msg) => {
 client.on('message', (msg) => {
   if (msg.content === '!end') {
     gameState.status = false;
+
+    db.run(`DELETE FROM users;`, (err) => {
+      if (err) {
+        console.log(err.message);
+      }
+    });
+  }
+});
+
+client.on('message', (msg) => {
+  if (msg.content === '!poker') {
+    db.get(
+      `SELECT * FROM profiles WHERE name='${msg.author.username}';`,
+      (err, name) => {
+        if (err) {
+          return console.log(err.message);
+        }
+
+        if (name === undefined) {
+          db.run(
+            'INSERT INTO profiles(name, chips) VALUES (?,?)',
+            [msg.author.username, 0],
+            (err) => {
+              if (err) {
+                return console.log(err.content);
+              }
+              msg.channel.send(`Profile created for ${msg.author.username}!`);
+            }
+          );
+        }
+      }
+    );
+  }
+});
+
+client.on('message', (msg) => {
+  if (msg.content === '!chips') {
+    db.get(
+      `SELECT chips FROM profiles WHERE name='${msg.author.username}';`,
+      (err, chips) => {
+        if (err) {
+          return console.log(err.message);
+        }
+
+        msg.channel.send(`${msg.author.username} has ${chips.chips} chips!`);
+      }
+    );
+  }
+});
+
+client.on('message', (msg, value = 50) => {
+  if (msg.content === '!add') {
+    dbcommands.profileExists(db, msg.author.username).then((exists) => {
+      console.log(exists);
+      if (exists) {
+        dbcommands.setChips(db, msg.author.username, value);
+        msg.channel.send(`Added ${value} chips to your account!`);
+      } else {
+        msg.channel.send('You do not have a profile yet!');
+      }
+    });
+  }
+});
+
+//Sends private message to user
+client.on('message', (msg) => {
+  if (msg.content === '!dm') {
+    msg.author.createDM().then(() => {
+      const channel = client.channels.cache.find(
+        (channel) =>
+          channel.type === 'dm' &&
+          channel.recipient.username === msg.author.username
+      );
+      const pokerEmbed = new Discord.MessageEmbed().setThumbnail(ACE_OF_C);
+      client.channels.cache.get(channel.id).send(pokerEmbed);
+    });
   }
 });
 
