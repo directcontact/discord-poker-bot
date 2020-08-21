9567e65f8f696e03d9c92aa62150140bdf42cd50
Johnny <jguo45@gmail.com>
2020-08-21 00:47:58 -0400

    for jeff to debug

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 const Discord = require('discord.js');
-
-var gameState = {
-    status: false,
-}
 const fs = require('fs');
 const _ = require('lodash');
 const client = new Discord.Client();
 require('dotenv').config();
 const sqlite3 = require('sqlite3').verbose();
+const MAX_PLAYERS = 2;
+
+var gameState = {
+    status: false,
+}
 
 let db = new sqlite3.Database(':memory:', (err) => {
   if (err) {
@@ -18,7 +19,7 @@ let db = new sqlite3.Database(':memory:', (err) => {
 });
 
 db.run(
-  'CREATE TABLE users (id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE, name TEXT, count INTEGER);',
+  'CREATE TABLE users (id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE, name TEXT, chips INTEGER);',
   (err) => {
     if (err) {
       return console.error(err.message);
@@ -28,10 +29,12 @@ db.run(
   }
 );
 
+// Login
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
+// Display pfp
 client.on('message', (msg) => {
   const split = msg.content.split(' ');
   if (split[0] === '!avatar') {
@@ -42,13 +45,15 @@ client.on('message', (msg) => {
   }
 });
 
-client.on('message', (msg) => {
-    if (msg.content === 'yeet') {
-        msg.reply('yoot');
-        console.log(msg.author.toString())
-    }
-})
+// Yeet test
+// client.on('message', (msg) => {
+//     if (msg.content === 'yeet') {
+//         msg.reply('yoot');
+//         console.log(msg.author.toString())
+//     }
+// })
 
+// Start poker game
 client.on('message', (msg) => {
     // const channel = msg.channel;
     if (msg.content === '!start') {
@@ -60,34 +65,121 @@ client.on('message', (msg) => {
     }
 })
 
+// Join poker game
 client.on('message', (msg) => {
     // const channel = client.channels.cache.find(ch => ch.type === 'text' && ch.name === 'general');
-    const channel = msg.channel;
+    // const channel = msg.channel;
     //console.log(`Before: ${gameState.status}`);
 
     if (msg.author.bot) return;
     else if (msg.content === '!play') {
         if (gameState.status === true) {
-            channel.send('Someone joined!');
+            let players = [];
+            // msg.channel.send('Someone joined!');
+            
+            //Adds user to poker table
+            db.get(
+                // `SELECT * FROM users WHERE name= ${msg.author.username}`, (err, name) => {
+                //     console.log(name);
+                //     if (name === undefined) {
+                //         db.run(
+                //             'INSERT INTO users(name, chips) VALUES (?,?)',
+                //             [msg.author.username, 0],
+                //             (err) => {
+                //                 if (err) {
+                //                     console.log(err.stack);
+                //                 }
+                //                 msg.channel.send('1234')
+                //                 // msg.channel.send('You are the first to join!');
+    
+                //             })
+                //     }
+                //     else if (name !== undefined) {
+                //         msg.channel.send('Youre already in');
+                //     }
+                // }
+
+                `SELECT COUNT(DISTINCT name) FROM users;`, (err, num) => {
+                    if (err) {
+                        console.log(err.message);
+                    }
+                    console.log(`Before: ${msg.author.username}|${num[Object.keys(num)[0]]}`)
+                    if (num[Object.keys(num)[0]] === MAX_PLAYERS) {
+                        msg.channel.send('The game is full');
+                    }
+                    else {
+                        db.get(
+                        `SELECT name FROM users WHERE name= ${msg.author.username}`, (err, name) => {
+                            console.log(name);
+                            if (name === undefined) {
+                                db.run(
+                                    'INSERT INTO users(name, chips) VALUES (?,?)',
+                                    [msg.author.username, 0],
+                                    (err) => {
+                                        if (err) {
+                                            console.log(err.stack);
+                                        }
+                                        msg.channel.send(`(${num[Object.keys(num)[0]] + 1}/${MAX_PLAYERS})`)
+                                        // msg.channel.send('You are the first to join!');
+            
+                                    })
+                            }
+                            else if (name !== undefined) {
+                                msg.channel.send('Youre already in');
+                            }
+                        })
+                        console.log(`After: ${msg.author.username}|${num[Object.keys(num)[0]]}`)
+
+
+                        // db.run(
+                        // 'INSERT INTO users(name, chips) VALUES (?,?)',
+                        // [msg.author.username, 0],
+                        // (err) => {
+                        //     if (err) {
+                        //         console.log(err.stack);
+                        //     }
+                        //     // msg.channel.send('You are the first to join!');
+
+                        // })
+                    }
+                }
+            );
         }
         else if (gameState.status === false) {
-            channel.send('A game hasnt started stupid');
+            msg.channel.send('A game hasnt started stupid');
         }
     }
     // console.log(`${gameState.status}`);
 })
 
 client.on('message', (msg) => {
+    if (msg.content === '!stats') {
+      db.all(`SELECT * from users;`, (err, rows) => {
+        if (err) {
+          console.error(err.message);
+        } else {
+          rows.map((user) => {
+            msg.reply(`${user.name} has ${user.count} replies`);
+          });
+        }
+      });
+    }
+  });
+
+// End poker game
+client.on('message', (msg) => {
     if (msg.content === '!end') {
         gameState.status = false;
     }
 })
 
+// Purge messages
 client.on('message', (msg, value = 1) => {
     if (msg.content === '!purge') {
         console.log(msg.channel.messages.cache)
     }
 })
+
 
 client.on('guildMemberAdd', member => {
     // Send the message to a designated channel on a server:
@@ -98,30 +190,18 @@ client.on('guildMemberAdd', member => {
     channel.send(`Oh god, it's ${member}`);
   });
 
-  client.on('message', (msg) => {
-    db.run(
-      'INSERT INTO users(name, count) VALUES (?,?)',
-      [msg.author.username, 1],
-      (err) => {
-        if (err) {
-          console.log(err.stack);
-        }
-      }
-    );
-  });
+//   client.on('message', (msg) => {
+//     db.run(
+//       'INSERT INTO users(name, count) VALUES (?,?)',
+//       [msg.author.username, 1],
+//       (err) => {
+//         if (err) {
+//           console.log(err.stack);
+//         }
+//       }
+//     );
+//   });
 
-client.on('message', (msg) => {
-  if (msg.content === '!stats') {
-    db.all(`SELECT * from users;`, (err, rows) => {
-      if (err) {
-        console.error(err.message);
-      } else {
-        rows.map((user) => {
-          msg.reply(`${user.name} has ${user.count} replies`);
-        });
-      }
-    });
-  }
-});
+
 
 client.login(process.env.CLIENT_ID);
